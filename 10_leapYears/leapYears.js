c933eb28b6479e0c226a1e74ad5f10a5ac0599af
Alisam17 <sameerali17@outlook.com>
2025-10-23 14:27:51 -0700

    finished lessons 6-11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,10 +1,17 @@
-const leapYears = function(year) {
+const leapYears = function (year) {
 
     //leap year is as such
     // divisible by 4
     // divisible by 100 if they are also divisble by 400
 
-    if ((year % 4 === 0) || (year % 400 === 0)) {
+    //1900 and 700 are failing.
+
+
+    if (year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
         return true;
     } else {
         return false;
@@ -12,5 +19,14 @@ const leapYears = function(year) {
 
 };
 
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// leap years are years divisible by 4
+// years divisble by 100 are not leap years unless they are divisible by 400 as well
+
+// by 4 is the main check. 
+// secondary check is divide by 100
+// third check is divide by 400
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+
+  if (farenheit === 32) {
+    return 0;
+  }
+
+  let celsius = 0;
+  celsius = Number(((farenheit - 32) * (5/9)).toFixed(1));
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  if (celsius === 0) {
+    return 32;
+  }
+
+  let farenheit = 0;
+  farenheit = Number(((celsius * (9/5)) + 32).toFixed(1));
+  return farenheit;
 };
 
 // Do not edit below this line

e091b4ac1b43601c13d34497563315ac61108988
Alisam17 <sameerali17@outlook.com>
2025-11-13 16:34:16 -0800

    finished ex 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, current) => accum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, current) => accum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	counter = 1;
+  while ( a > 0) {
+    counter *= a;
+    a = a - 1;
+  }
+  return counter;
 };
 
 // Do not edit below this line

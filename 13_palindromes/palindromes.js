9c29dffeb32796b46d565dc2ba0663d953eb9baa
Alisam17 <sameerali17@outlook.com>
2025-11-13 17:46:58 -0800

    finished up to and including exercise 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumeric = str.replace(/[^a-zA-Z0-9]/g, '');
+    let splitStr = alphanumeric.split('');
+    splitStr.reverse();
+    let sortedStr = splitStr.join('');
+    if(alphanumeric.toLowerCase() == sortedStr.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+function getAge(people) {
+    const currentYear = new Date().getFullYear();
+    if (people.yearOfDeath == undefined) {
+        return (currentYear - people.yearOfBirth)
+    } else {
+        return people.yearOfDeath - people.yearOfBirth;
+    }
+}
 
+const findTheOldest = function (people) {
+
+    if (people.size < 2) {
+        return people[0];
+    } else if (people.size == 0) {
+        return [""];
+    }
+    let arr = people.reduce((oldest, currentPerson) => {
+        let currAge = getAge(currentPerson);
+        let oldestAge = getAge(oldest);
+        if (currAge > oldestAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+
+        
+    }, people[0])
+
+    return arr;
 };
 
 // Do not edit below this line
